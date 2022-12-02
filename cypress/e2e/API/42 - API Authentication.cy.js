8caa1bad92fb66fa8db94588d4629b9772f49b2d
mzwwy <wahyucheger51@gmail.com>
2022-12-02 17:59:47 +0700

    commit

--- cypress/e2e/API/42 - API Authentication.cy.js
+++ cypress/e2e/API/42 - API Authentication.cy.js
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
-describe('Custom Commands', () => {
-=======
 describe('Basic Auth', () => {
->>>>>>> d45f513157e8710dbfdb320006111f254486d7b1
     it('Successfully login by appending username and password in URL', function () {
         cy.visit('https://admin:admin@the-internet.herokuapp.com/basic_auth')
         cy.get('p').should('include.text', 'Congratulations! You must have the proper credentials.')
     })
-<<<<<<< HEAD
-=======
 
->>>>>>> d45f513157e8710dbfdb320006111f254486d7b1
     it('Successfully login using headers', function () {
         cy.visit("https://the-internet.herokuapp.com/basic_auth", {
             headers: {
@@ -20,8 +13,5 @@ describe('Basic Auth', () => {
         })
         cy.get('p').should('include.text', 'Congratulations! You must have the proper credentials.')
     })
-<<<<<<< HEAD
-=======
  
->>>>>>> d45f513157e8710dbfdb320006111f254486d7b1
 });
